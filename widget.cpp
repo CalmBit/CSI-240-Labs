4ac8e3ff913028542c93cb0e5b87792e3175b137
Ethan Brooks <EB5473@gmail.com>
2016-02-25 11:10:57 -0500

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -1,3 +1,16 @@
+/*
+  Author:  Ethan Brooks
+  Class:   CSI-240-02
+  Assignment:  Lab 6
+  Date Assigned: 2016-02-22
+  Due Date:  2016-02-25, 11 AM
+  Description:  Creates and copies Widget objects.
+  Certification of Authenticity: 
+  I certify that this is entirely my own work, except where I have given fully-documented references to the work of others. I understand the definition and consequences of plagiarism and acknowledge that the assessor of this assignment may, for the purpose of assessing this assignment:
+      - Reproduce this assignment and provide a copy to another member of academic staff; and/or
+      - Communicate a copy of this assignment to a plagiarism checking service (which may then retain a copy of this assignment on its database for the purpose of future plagiarism checking)
+*/
+
 #include "widget.h"
 
 #include <sstream>
