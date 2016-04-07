6f52413113aff4e87c23a9816087624420e02e71
Ethan Brooks <EB5473@gmail.com>
2016-04-07 14:01:42 -0400

    Added person class

---
+++ Person.cpp
@@ -0,0 +1,20 @@
+#include "Person.hpp"
+
+#include <iostream>
+
+Person::Person()
+{
+  setName("");
+  std::cout << "Person object created using the default Person constructor." << std::endl;
+}
+
+Person::Person(std::string name)
+{
+  setName(name);
+  std::cout << "Person object created using the alternate Person constructor." << std::endl;
+}
+
+~Person::Person()
+{
+  std::cout << "Person object destroyed!" << std::endl;
+}
